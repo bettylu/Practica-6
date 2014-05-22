499b0f121a3d516bcaebd585da8dc086a608879e
bettylu <btty.deluna@gmail.com>
2014-05-22 02:06:36 -0500

    MySQL

---
+++ MySQL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Practica_6
+{
+	/// <summary>
+	/// Description of MySQL.
+	/// </summary>
+	public class MySQL
+	{
+		public MySqlConnection myConnection;
+		public MySQL ()
+		{
+		}
+
+		public void abrirConexion(){
+			string connectionString =
+           	"Server=localhost;" +
+			"Database=profesores;" +
+			"User ID=root;" +
+			"Password=1234;" +
+			"Pooling=false;";
+			this.myConnection = new MySqlConnection(connectionString);
+			this.myConnection.Open();
+		}
+
+		public void cerrarConexion(){
+			this.myConnection.Close();
+			this.myConnection = null;
+		}
+	}
+}
+
